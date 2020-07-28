a1a54f5195bce80218c04f57d7b8b5624cc2a93b
LifeStly <45162527+LifeStly@users.noreply.github.com>
2020-07-28 12:43:36 +0700

    save

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
